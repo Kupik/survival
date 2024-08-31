9df5a55770879ab0d1613af3efad90aaa6f5902b
Kupik <kabutaistrate@gmail.com>
2024-08-31 06:08:11 -0700

    Add layer menu 1.1v

---
+++ Assets/MenuController/ControlllScripMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlllScripMenu : MonoBehaviour
+{
+    public Button ButtonActive; // Butonul de activare
+    public Button button1; // Primul buton care trebuie să apară
+    public Button button2; // Al doilea buton care trebuie să apară
+
+    public void Start()
+    {
+        
+    }
+
+    public void ActiveOrNoActive()
+    {
+        button1.gameObject.SetActive(true);
+        button2.gameObject.SetActive(true);
+    }
+
+}
